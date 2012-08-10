a3976a2c50a8be7619d580a6ab83f2689c0c2beb
cho45 <cho45@lowreal.net>
2012-08-11 01:03:10 +0900

    適度にとぎれたりするように

--- sketch/node.js
+++ sketch/node.js
@@ -24,7 +24,7 @@ Array.apply(null, new Array(40)).forEach(function (_, i) {
 		var taken = method == 'GET' ? nrand(200, 700) : nrand(500, 3000);
 		if (taken < 0) taken = 0;
 
-		var path  = Math.floor(nrand(0, 1));
+		var path  = Math.floor(nrand(0, 3));
 		if (path < 0) path = 0;
 		if (path > (paths.length - 1)) path = 0;
 
@@ -32,13 +32,18 @@ Array.apply(null, new Array(40)).forEach(function (_, i) {
 			"time:" + new Date().toString(),
 			"req:" + method + " " + paths[path] + " HTTP/1.0",
 			"addr:127.0.0." + i,
-			"status:" + (Math.random() < 0.05 ? 500 : (Math.floor(Math.random() * 2 + 2) * 100)),
+			"status:" + (Math.random() < 0.05 ? 500 : Math.random() < 0.15 ? 302 : 200),
 			"taken:" + taken,
 			"ua:stdin"
 		].join("\t"));
 
 		var wait = nrand(5000, 5000);
 		if (wait < 0) wait = 0;
+
+		if (Math.random() < 0.30) {
+			i = Math.floor(Math.random() * 255);
+		}
+
 		setTimeout(arguments.callee, wait);
 	})();
 });
