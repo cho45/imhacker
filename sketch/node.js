257ea2045d094b15efd7f7ac2d81b1bf1d3bb1a2
cho45 <cho45@lowreal.net>
2012-08-11 00:04:45 +0900

    dummy data

--- sketch/node.js
+++ sketch/node.js
@@ -1,15 +1,16 @@
 #!node
 
-(function () {
-	console.log([
-		"time:" + new Date().toString(),
-		"req:GET / HTTP/1.0",
-		"addr:127.0.0.1",
-		"status:" + Math.floor(Math.random() * 3 + 2) * 100,
-		"taken:" + Math.random() * 3,
-		"ua:stdin"
-	].join("\t"));
+Array.apply(null, new Array(30)).forEach(function (_, i) {
+	(function () {
+		console.log([
+			"time:" + new Date().toString(),
+			"req:GET / HTTP/1.0",
+			"addr:127.0.0." + i,
+			"status:" + Math.floor(Math.random() * 3 + 2) * 100,
+			"taken:" + Math.random() * 3,
+			"ua:stdin"
+		].join("\t"));
 
-	setTimeout(arguments.callee, Math.random() * 3000);
-})();
-
+		setTimeout(arguments.callee, Math.random() < 0.9 ? Math.random() * 5000 : 650000);
+	})();
+});
