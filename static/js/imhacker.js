a000b0276e1190d913a129efeab17928ac087654
cho45 <cho45@lowreal.net>
2012-08-11 11:50:25 +0900

    demo mode

--- static/js/imhacker.js
+++ static/js/imhacker.js
@@ -80,6 +80,10 @@ ImHacker = {
 		self.drawGraphs();
 
 		// $('#windows').find('.btn[data-num=0]').click();
+
+		if (location.hash == '#demo') {
+			self.startDemo();
+		}
 	},
 
 	initStats : function () {
@@ -131,6 +135,61 @@ ImHacker = {
 
 		self.timeThreshold = +localStorage.timeThreshold;
 		self.ignorePath    = new RegExp(localStorage.ignorePath);
+	},
+
+	startDemo : function () {
+		var self = this;
+
+		var paths = [
+			'/',
+			'/api/',
+			'/api/get',
+			'/api/post',
+			'/api/user',
+			'/log',
+			'/2012/',
+			'/user/',
+			'/entry/',
+			'/entry/foobar',
+			'/entry/barbaz'
+		];
+
+		function ndrand (u, s) {
+			return Math.sqrt(-2 * Math.log(Math.random())) * Math.cos(2 * Math.PI * Math.random()) * s + u;
+		}
+
+		function lndrand (u, s) {
+			return Math.exp(ndrand(Math.log(u * u) - Math.log(u * u + s * s) / 2.0, Math.sqrt(Math.log(1 + s / u * s / u))));
+		}
+
+		Array.apply(null, new Array(40)).forEach(function (_, i) {
+			(function () {
+				var method = (Math.random() < 0.1 ? 'POST' : 'GET');
+				var taken = method == 'GET' ? lndrand(200, 700) : lndrand(500, 3000);
+
+				var path  = Math.floor(ndrand(0, 3));
+				if (path < 0) path = 0;
+				if (path > (paths.length - 1)) path = 0;
+
+				self.updateResponseStats({
+					datetime : new Date(),
+					method   : method,
+					path     : paths[path],
+					addr     : "127.0.0." + i,
+					code     : (Math.random() < 0.05 ? 500 : Math.random() < 0.15 ? 302 : 200),
+					millisec : taken,
+					ua       : 'demo'
+				});
+
+				var wait = lndrand(5000, 5000);
+
+				if (Math.random() < 0.50) {
+					i = Math.floor(Math.random() * 255);
+				}
+
+				setTimeout(arguments.callee, wait);
+			})();
+		});
 	},
 
 	prepareSocket : function () {
